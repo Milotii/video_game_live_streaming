1558a08a5eca4758622cc04a6aa2f34db925004b
DESKTOP-TVK982O\Lenovo <milothoti10@gmail.com>
2023-02-08 02:02:50 +0100

    no one can sign in without having an account, and i made logout option

--- home/home.php
+++ home/home.php
@@ -1,3 +1,16 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: ../login/login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: ../login/login.php");
+  }
+?>
 <!DOCTYPE html>
 <html>
 
@@ -33,8 +46,26 @@
         </button>
       </div>
     </div>
-
     <a href="../login/login.php" class="log-in">Log In</a>
+    <br>
+    <div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="home.php?logout='1'" style="color: red;">Logout</a> </p>
+    <?php endif ?>
 
   </header>
   <div id="slider">
