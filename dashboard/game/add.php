8d23f6f54f01f27367d3cc5116116ede5bb18821
Fisnik Ibishi <fisnikibishi@gmail.com>
2023-02-11 18:50:05 +0100

    Added menu.php and add.php files

---
+++ dashboard/game/add.php
@@ -0,0 +1,138 @@
+<?php
+// Establish a connection to the database
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "videogamelivestreaming_db";
+
+try {
+  $con = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Connection Failed " . $e->getMessage();
+}
+
+// Check if the form has been submitted
+if (isset($_POST['submit'])) {
+  // Get the form data
+  $name = $_POST['name'];
+  $description = $_POST['description'];
+  $image = file_get_contents($_FILES['image']['tmp_name']);
+  $video = file_get_contents($_FILES['video']['tmp_name']);
+
+  // Insert data into the categories table
+  try {
+    // Prepare the SQL statement
+    $stmt = $con->prepare("INSERT INTO categories (name, description, image, video) VALUES (:name, :description, :image, :video)");
+
+    // Bind the values to the placeholders in the SQL statement
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':description', $description, PDO::PARAM_STR);
+    $stmt->bindValue(':image', $image, PDO::PARAM_LOB);
+    $stmt->bindValue(':video', $video, PDO::PARAM_LOB);
+
+    // Execute the SQL statement
+    $stmt->execute();
+
+    echo "Data inserted successfully";
+  } catch (PDOException $e) {
+    echo "Insert failed: " . $e->getMessage();
+  }
+}
+?>
+<!DOCTYPE html>
+<!-- Designined by CodingLab | www.youtube.com/codinglabyt -->
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../dashboard.css">
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    form {
+      width: 500px;
+      margin: 0 auto;
+      padding: 20px;
+      border: 1px solid #ccc;
+    }
+
+    label {
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    input[type="text"],
+    input[type="file"] {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 20px;
+      box-sizing: border-box;
+      border: 1px solid #ccc;
+    }
+
+    input[type="submit"] {
+      width: 100%;
+      padding: 10px;
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      cursor: pointer;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include '../menu.php'; ?>
+
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+      <div class="search-box">
+        <input type="text" placeholder="Search...">
+        <i class='bx bx-search'></i>
+      </div>
+      <div class="profile-details">
+        <!--<img src="images/profile.jpg" alt="">-->
+        <span class="admin_name">Milot Hoti</span>
+        <i class='bx bx-chevron-down'></i>
+
+      </div>
+    </nav>
+    <div class="home-content">
+      <div class="overview-boxes">
+        <form action="" method="post" enctype="multipart/form-data">
+          <label for="name">Name:</label>
+          <input type="text" name="name" id="name">
+
+          <label for="description">Description:</label>
+          <input type="text" name="description" id="description">
+
+          <label for="image">Image:</label>
+          <input type="file" name="image" id="image">
+
+          <label for="video">Video:</label>
+          <input type="file" name="video" id="video">
+
+          <input type="submit" name="submit" value="Submit">
+        </form>
+  </section>
+
+  <script>
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".sidebarBtn");
+    sidebarBtn.onclick = function () {
+      sidebar.classList.toggle("active");
+      if (sidebar.classList.contains("active")) {
+        sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      } else
+        sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+  </script>
+
+</body>
+
+</html>
